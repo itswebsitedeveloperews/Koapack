a7f40caf2f1f386b29ca9dfb208d2215e01f91e0
ews-site <ajaypadhiyar.ews@gmail.com>
2026-05-22 15:18:03 +0530

    a

--- product-options-manager/app/routes/auth.$.jsx
+++ product-options-manager/app/routes/auth.$.jsx
@@ -2,9 +2,9 @@ import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  const { redirect } = await authenticate.admin(request);
+  await authenticate.admin(request);
 
-  return redirect("/app");
+  return null;
 };
 
 export const headers = (headersArgs) => {
